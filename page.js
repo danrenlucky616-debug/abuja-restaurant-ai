3bc11d50384aec5f5fb5a28cf528ba7a49b05f8f
danrenlucky616-debug <danrenlucky616@gmail.com>
2026-08-26 23:05:40 +0100

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div style={{padding: 40, maxWidth: 800, margin: '0 auto'}}>
-      <h1>Admin Dashboard</h1>
-      <p>Welcome! Restaurant settings and orders will go here later.</p>
-    </div>
-  )
-}
